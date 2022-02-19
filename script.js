f662cff631bfa8dd4eed7a46b152fda0d9f56c5a
klezi10 <74952593+klezi10@users.noreply.github.com>
2022-02-19 15:50:30 +0700

    set up styling and the gallery

---
+++ script.js
@@ -0,0 +1,28 @@
+async function getPhotos() {
+  let response = await fetch('photos.json');
+  let photos = await response.json();
+  return photos;
+}
+
+function showPhotos(photos) {
+  const renderPhotos = photos
+    .map((photo) => {
+      return `
+    <img class="my-photo" src="https://picsum.photos/id/${photo.id}/100/100" alt="${photos.title}" />
+    `;
+    })
+    .join('');
+  return `
+    <div class="my-photos">${renderPhotos}</div>
+    `;
+}
+
+getPhotos().then((photos) => {
+  const renderPhotos = showPhotos(photos);
+  document.body.innerHTML = `
+  <div class="my-gallery">
+  <img id="my-selected-photo" class="my-photo" src="https://picsum.photos/id/1/300/300" />
+  ${renderPhotos}
+  </div>
+  `;
+});
